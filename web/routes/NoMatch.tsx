ee668cea164a5e162acb967b66b6b812faf81740
KingMob <john@onethreethree.net>
2024-11-14 00:02:09 +0000

    add title and link to no match page

--- web/routes/NoMatch.tsx
+++ web/routes/NoMatch.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
+
 export default function NoMatch() {
   return (
     <div>
+      <Header />
+      <h1>No page found</h1>
       <img src="/nightmare.jpeg" />
+      <Link to="/">
+        <p>back to main page</p>
+      </Link>
     </div>
   );
 }
